addc34c8baa9b025c11fec8bfa8af6f177f02847
spezifisch <spezifisch@users.noreply.github.com>
2021-11-17 12:55:57 +0100

    increment app version

--- radeon-profile-daemon/rpdthread.h
+++ radeon-profile-daemon/rpdthread.h
@@ -30,7 +30,7 @@
 #define SIGNAL_SHAREDMEM_KEY '6'
 #define SIGNAL_ALIVE '7'
 
-const QString appVersion = "20190603";
+const QString appVersion = "20211116";
 const QString serverSocketPath = "/run/radeon-profile-daemon/radeon-profile-daemon-server";
 
 class rpdThread : public QThread
